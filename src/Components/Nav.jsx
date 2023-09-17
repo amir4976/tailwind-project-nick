384ca7da44e39fc77ae0e571f7aab8e6f456a4b7
amir4976 <amirhosianahmadi4976@gmail.com>
2023-09-17 23:23:24 +0330

    hero section

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import {headerLogo} from '../assets/images'
 import {hamburger} from '../assets/icons'
+import { navLinks } from '../constants'
+
 function Nav() {
   return (
     <div>
@@ -9,9 +11,18 @@ function Nav() {
                 <a href="/">
                     <img src={headerLogo} alt="logo" width={130} height={29} />
                 </a>
-                <ul className='flex-1 justify-center items-center gap-16 max-lg:hidden'>
-                    
+                <ul className='flex justify-center items-center gap-16 max-lg:hidden'>
+                    {navLinks.map((item)=>(
+                        <li key={item.label}>
+                            <a href={item.href} className='font-montserrat leading-normal text-lg text-slate-gary'>
+                                {item.label}
+                            </a>
+                        </li>
+                    ))}
                 </ul>
+                <div className='hidden max-lg:block'>
+                    <img src={hamburger} alt="hamburger" width={25} height={25}/>
+                </div>
             </nav>
       </header>
     </div>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
 
+import Button from "../Components/Button";
+import { arrowRight } from "../assets/icons";
+import { statistics } from "../constants";
 function Hero() {
   return (
-    <div>
-      
-    </div>
-  )
+    <section
+      id="home"
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-contianer border-2 border-red-500 p-2"
+    >
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+        <p className="text-xl font-montserrat">Our summer collection</p>
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">them new Arruval</span>
+          <br />
+          <span>Nike </span>
+          Shoes
+        </h1>
+        <p>
+          Discover Stylesh nike arrivals quality comfort and innovation for
+          active life
+        </p>
+        <Button lable={"Show Now"} iconUrl={arrowRight} />
+
+        <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
+            {statistics.map((stat,index)=>(
+              <div key={index}>
+              <p>{stat.value}</p>
+              <p>{stat.label}</p>
+              </div>
+            ))}
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Hero
+export default Hero;
