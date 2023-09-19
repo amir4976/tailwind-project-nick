8b3c03986c702cf0a76391fedd508a6a3b918e29
amir4976 <amirhosianahmadi4976@gmail.com>
2023-09-19 22:11:03 +0330

    popular card

--- src/Components/PopularProductCard.jsx
+++ src/Components/PopularProductCard.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import { star } from "../assets/icons";
 
-function PopularProductCard({imgURL,name,price}) {
+function PopularProductCard({ imgURL, name, price }) {
   return (
-    <div className='flex flex-1 flex-col w-full max-sm:w-full'>
-   <img src={imgURL} alt={name} className='w-[280px] h-[280px]'/>
+    <div className="flex flex-1 flex-col w-full max-sm:w-full">
+      <img src={imgURL} alt={name} className="w-[280px] h-[280px]" />
+      <div className="mt-8 flex justify-start gap-2.5">
+        <img src={star} alt="rating" width={24} height={24} />
+        <p className="font-montserrat text-xl leading-normal text-slate-gray">
+          4.5
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default PopularProductCard
+export default PopularProductCard;
